d9364dca4c861b6949f05a913f127554c49e8c20
Abdul Rehman <71893203+Git-EXPifani@users.noreply.github.com>
2025-10-28 07:44:38 +0500

    Added two functions with the game working
    A bit of html for guidance was added too :D

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let userInput = prompt("Enter your choice: ");
-    console.log(userInput);
+    return userInput;
 }
 
 function playRound(humChoice,comChoice) {
@@ -40,6 +40,66 @@ let theRest2 = theRest.toLowerCase();
     humChoice = firstLetter + theRest2;
     console.log(humChoice);
     console.log(comChoice);
+
+    if(humChoice==comChoice){//Same choice
+        return "No points for either!."
+    } else if(humChoice=="Paper"){//Player chooses Paper
+        if(comChoice=="Rock"){
+        humanScore++;
+        return "You Win! Paper beats Rock.";}
+            else if(comChoice=="Scissors"){
+                        computerScore++;
+                        return "You Lose! Paper loses to Scissors.";}}
+
+    //Player chooses Rock 
+    if(humChoice=="Rock"){
+        if(comChoice=="Scissors"){
+        humanScore++;
+        return "You Win! Rock beats Scissors.";
+    }else if(comChoice=="Paper"){
+        computerScore++;
+        return "You Lose! Rock loses to Paper.";
+    }}
+
+    //Player chooses Scissors
+     if(humChoice=="Scissors"){
+        if(comChoice=="Paper"){
+        humanScore++;
+        return "You Win! Scissors beat Paper.";
+    }else if(comChoice=="Rock"){
+        computerScore++;
+        return "You Lose! Scissors loses to Rock.";
+    }}       
+    
+}
+
+function playGame(){
+    let userChoice=getHumanChoice();
+    let comChoice=getComputerChoice();
+
+    console.log(playRound(userChoice,comChoice));
+    //One Round
+     userChoice=getHumanChoice();
+     comChoice=getComputerChoice();
+
+    console.log(playRound(userChoice,comChoice));
+    //One Round
+     userChoice=getHumanChoice();
+     comChoice=getComputerChoice();
+
+    console.log(playRound(userChoice,comChoice));
+    //One Round
+     userChoice=getHumanChoice();
+     comChoice=getComputerChoice();
+
+    console.log(playRound(userChoice,comChoice));
+    //One Round
+     userChoice=getHumanChoice();
+     comChoice=getComputerChoice();
+
+    console.log(playRound(userChoice,comChoice));
+    //One Round
+    console.log("5 Rounds over!")
 }
 
 
